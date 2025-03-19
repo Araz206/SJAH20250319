9b0fc416a5bbedbb98e71f247176f9c8e8072218
Sarai Jemima Arenivar Hernandez <6796008@clases.edu.sv>
2025-03-19 08:08:52 -0600

    Agregar archivos de proyecto.

---
+++ SJAH20250319/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SJAH20250319.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
